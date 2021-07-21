064de90e78d85942af031354d2d46be4f12fb08e
anshita <anshitasaini@gmail.com>
2021-07-21 21:00:08 +0000

    independent practice #spicy

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,3 +1,8 @@
+passInput = document.querySelector("#passcodeInput")
+printMessage = document.querySelector('#message')
+another = document.querySelector("#another")
+messageContainer = document.querySelector("#messageContainer")
+
 const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
@@ -7,14 +12,17 @@ const getMessages = () => {
             const message = data[key];
             console.log(message);
             if (myPass == message.passcode) {
-                renderMessage(message);
+                renderMessageAsHTML(message);
             }       
         }
     });
 
 }
 
+let attempts = 0;
+
 const findMessage = (myPass) => {
+    let found = 0;
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
@@ -23,13 +31,48 @@ const findMessage = (myPass) => {
             const message = data[key];
             console.log(message);
             if (myPass == message.passcode) {
-                renderMessage(message);
-            }       
+                renderMessageAsHTML(message);
+                found += 1
+            }     
+        }
+        if (found == 0) {
+            alert("Passcode does not match any messages!")
+        }
+        if (attempts >= 5) {
+            alert ("5 attempts are up!")
+            document.querySelector("#passcode").disabled = true;
+            document.querySelector("#viewMsg").disabled = true;
+            setTimeout(refresh, 3000)
         }
     });
 }
 
 document.querySelector("#viewMsg").addEventListener("click", () => {
     const passcode = document.querySelector('#passcode').value;
+    attempts += 1;
     findMessage(passcode);
 });
+
+document.querySelector("#another").addEventListener("click", () => {
+    passInput.style.display = 'block';
+    printMessage.innerHTML = '';
+    another.style.display = 'none';
+    messageContainer.style.display = 'none';
+    attempts = 0;
+});
+
+
+renderMessageAsHTML = (message) => {
+    passInput.style.display = 'none';
+    messageContainer.style.display = 'block';
+    printMessage.innerHTML = message.message;
+    another.style.display = 'block';
+}
+
+
+refresh = () => {
+    attempts = 0;
+    document.querySelector("#passcode").disabled = false;
+    document.querySelector("#viewMsg").disabled = false;
+    alert("You can try again now.")
+}
